3d9644192766f40b602c08ee2bb4192cb32059ce
alu10194039 <aluurs4039@ieselcaminas.org>
2021-09-29 18:35:52 +0200

    Final Commit

--- app/src/main/java/com/example/entradadatosportecladoconsolakotlin/MainActivity.kt
+++ app/src/main/java/com/example/entradadatosportecladoconsolakotlin/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,19 +18,27 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.bt1)
         val et1 = findViewById<EditText>(R.id.et1)
         val et2 = findViewById<EditText>(R.id.et2)
+        val et3 = findViewById<EditText>(R.id.et3)
+        val et4 = findViewById<EditText>(R.id.et4)
 
+        //et1.doAfterTextChanged {
+
+        //}
         button.setOnClickListener {
 
             val primerNumero = et1.text.toString().toFloat()
             val segundoNumero = et2.text.toString().toFloat()
+            val tercerNumero = et3.text.toString().toFloat()
+            val cuartoNumero = et4.text.toString().toFloat()
 
-            val sumatorio = primerNumero + segundoNumero
-            println("El sumatorio equivale a: \n $sumatorio")
-            val producto = primerNumero * segundoNumero
-            println("El sumatorio equivale a: \n $producto")
+            val totalSuma = primerNumero + segundoNumero + tercerNumero + cuartoNumero
+            val media = totalSuma / 4
+
+            println("El sumatorio equivale a: \n $totalSuma")
+            println("La media equivale a: \n $media")
 
             val tv1 = findViewById<TextView>(R.id.tv1).apply {
-                text = ("El sumatorio equivale a: $sumatorio \n y el producto equivale a: $producto ").toString()
+                text = ("El sumatorio equivale a: $totalSuma\nLa media equivale a: $media").toString()
             }
         }
     }
